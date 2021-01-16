746b5448aad81f58c2af34042c99a1f7c3990733
zeeteexx <77551399+zeeteexx@users.noreply.github.com>
2021-01-17 03:13:31 +0700

    Add files via upload

---
+++ trojanzee.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+clear
+echo -e "\e[91m"
+figlet "Z-TX TOOLS"
+echo -e "\e[33m===================="
+echo -e "\e[95mAUTHOR = Zee TeExx"
+echo -e "\e[95mYOUTUBE = Z-TX "
+echo -e "\e[33m===================="
+
+
+sleep 2
+echo -e "\e[91m"
+read -p 'NOMOR HP TARGET = ' nomor
+while [[ true ]]
+do
+echo -e "\e[38;5;82mVirus Telah di Kirim By Zee TeExx"
+echo -e "\e[1;37m"
+done
